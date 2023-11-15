e68e7f3a43ff1f71ac07f81808673b10c9786e2e
5nam <ohnam90@gmail.com>
2023-11-15 11:51:34 +0900

    [feat] : 테스트 conveter 생성

--- src/main/java/umc/spring/converter/TempConverter.java
+++ src/main/java/umc/spring/converter/TempConverter.java
@@ -1,2 +1,12 @@
-package umc.spring.converter;public class TempConverter {
+package umc.spring.converter;
+
+import umc.spring.web.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO() {
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is Test!")
+                .build();
+    }
 }
