2b1c832e51aaac6ebb4ef8971944ec9f56c4661a
5nam <ohnam90@gmail.com>
2023-11-15 11:47:50 +0900

    [feat] : 응답 json 공통적으로 생성

---
+++ src/main/java/umc/spring/base/Code.java
@@ -0,0 +1,28 @@
+package umc.spring.base;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum Code {
+    OK(HttpStatus.OK, "2000", "Ok");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+}
