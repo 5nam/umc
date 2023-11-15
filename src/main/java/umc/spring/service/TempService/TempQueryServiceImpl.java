9d5f8a802ff4d6a4d7bf5022dbc010e11f7f654f
5nam <ohnam90@gmail.com>
2023-11-15 12:59:00 +0900

    [fix] : 'umc.spring.service.TempService.TempQueryService' that could not be found - @Service, @RequiredArgsConstructor 을 안적어서 Bean 으로 등록되지 않아서 오류 났음

--- src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
+++ src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
@@ -1,8 +1,12 @@
 package umc.spring.service.TempService;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import umc.spring.base.Code;
 import umc.spring.base.exception.handler.TempHandler;
 
+@Service
+@RequiredArgsConstructor
 public class TempQueryServiceImpl implements TempQueryService {
     @Override
     public void CheckFlag(Integer flag) {
