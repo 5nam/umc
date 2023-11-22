c2e83640dd5b06922ca29df789fabffbb0a257ad
5nam <ohnam90@gmail.com>
2023-11-22 16:27:06 +0900

    [feat] : 회원가입 Controller 완성(ResponseEntity -> ResponseDto 로 변경)

--- src/main/java/umc/spring/web/controller/MemberRestController.java
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -1,12 +1,14 @@
 package umc.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.spring.base.ResponseDto;
+import umc.spring.converter.MemberConverter;
+import umc.spring.domain.Member;
 import umc.spring.service.MemberService.MemberCommandService;
 import umc.spring.web.dto.MemberRequestDTO;
 import umc.spring.web.dto.MemberResponseDTO;
@@ -19,7 +21,8 @@ public class MemberRestController {
     private final MemberCommandService memberCommandService;
 
     @PostMapping("/")
-    public ResponseEntity<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Validated MemberRequestDTO.JoinDto request) {
-        return null;
+    public ResponseDto<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Validated MemberRequestDTO.JoinDto request) {
+        Member member = memberCommandService.joinMember(request);
+        return ResponseDto.onSuccess(MemberConverter.toJoinResultDTO(member));
     }
 }
