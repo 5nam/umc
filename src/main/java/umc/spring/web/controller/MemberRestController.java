088159b8e5fc5b368eb150af1cd44947adc19f11
5nam <ohnam90@gmail.com>
2023-11-22 15:56:52 +0900

    [feat] : 회원가입 Converter 에서 멤버 생성

---
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -0,0 +1,23 @@
+package umc.spring.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.web.dto.MemberRequestDTO;
+import umc.spring.web.dto.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Validated MemberRequestDTO.JoinDto request) {
+        return null;
+    }
+}
